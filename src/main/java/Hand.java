0c1132109f272a06168168ff426894e00fba2217
stefano-chen <stefanochen01@gmail.com>
2024-10-17 17:39:22 +0200

    checkRuns implemented

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -8,8 +8,8 @@ import java.util.Map;
 public class Hand {
 
     private final Card[] cards = new Card[4];
-    private Card starter;
     private final String strCard;
+    private Card starter;
 
     public Hand(String cards) throws CardException {
         this.strCard = cards;
@@ -26,6 +26,7 @@ public class Hand {
                 return c;
             }
         }
+
         return null;
     }
 
@@ -33,11 +34,13 @@ public class Hand {
     // otherwise it returns all the found cards
     public Card[] getCards(Rank rank) {
         ArrayList<Card> result = new ArrayList<>();
+
         for (Card c : this.cards) {
             if (c.sameRank(rank)) {
                 result.add(c);
             }
         }
+
         return result.isEmpty() ? null : result.toArray(new Card[result.size()]);
     }
 
@@ -46,11 +49,13 @@ public class Hand {
     // otherwise it returns all the found cards
     public Card[] getCards(Suit suit) {
         ArrayList<Card> result = new ArrayList<>();
+
         for (Card c : this.cards) {
             if (c.sameSuit(suit)) {
                 result.add(c);
             }
         }
+
         return result.isEmpty() ? null : result.toArray(new Card[result.size()]);
     }
 
@@ -58,18 +63,22 @@ public class Hand {
     // It creates an array containing the 4 hand cards and initialize the starter card
     private void fromString(String strCard) throws CardException {
         String[] token = strCard.split(" ");
+
         if (token.length != 5) {
             throw new CardException("Cards string not well formatted");
         }
+
         for (int i = 0; i < token.length - 1; i++) {
             this.cards[i] = Card.fromString(token[i]);
         }
+
         this.starter = Card.fromString(token[4]);
     }
 
     @Override
     public String toString() {
         String str = "";
+
         for (int i = 0; i < cards.length; i++) {
             str += ("hand card " + (i + 1) + System.lineSeparator() + ("    " + this.cards[i]) + System.lineSeparator());
         }
@@ -91,20 +100,28 @@ public class Hand {
     private int checkFlush() {
         int points = 0;
         Card[] handCards = this.getCards(this.cards[0].getSuit());
+
         if (handCards.length == 4) {
+
             points += 4;
             if (handCards[0].sameSuit(this.starter.getSuit())) {
                 points += 1;
             }
+
         }
+
         handCards = this.getCards(Rank.Jack);
+
         if (handCards != null) {
+
             for (Card c : handCards) {
                 if (c.sameSuit(this.starter.getSuit())) {
                     points += 1;
                 }
             }
+
         }
+
         return points;
     }
 
@@ -113,17 +130,24 @@ public class Hand {
     // 12 points for four cards with the same rank
     private int checkPairs() {
         int points = 0;
+
+        // Create a Dictionary with the number of occurrences per rank
         Map<Rank, Integer> hashmap = new HashMap<>();
+
         for (Card c : this.cards) {
+
             if (!hashmap.containsKey(c.getRank())) {
                 hashmap.put(c.getRank(), 1);
             } else {
                 Rank rank = c.getRank();
                 hashmap.put(rank, hashmap.get(rank) + 1);
             }
+
         }
 
+        // Assign points based on the number of occurrences
         for (Map.Entry<Rank, Integer> entry : hashmap.entrySet()) {
+
             switch (entry.getValue()) {
                 case 2:
                     points += 2;
@@ -135,7 +159,9 @@ public class Hand {
                     points += 12;
                     break;
             }
+
         }
+
         return points;
     }
 
@@ -146,22 +172,26 @@ public class Hand {
     public int checkRuns() {
         Card[] x = Arrays.copyOf(this.cards, 5);
         x[4] = this.starter;
-        Arrays.sort(x);
         // the greatest scale achieved at the moment
         int maxScaleLen = 1;
         int i = 0;
         // current number of cards at the moment in the scale
         int currScaleLen = 1;
 
+        Arrays.sort(x);
+
         while (i < x.length - 1) {
+
             if (x[i].getValue() == x[i + 1].getValue() - 1) {
                 currScaleLen += 1;
             } else {
                 maxScaleLen = Math.max(maxScaleLen, currScaleLen);
                 currScaleLen = 1;
             }
+
             i++;
         }
+
         maxScaleLen = Math.max(maxScaleLen, currScaleLen);
 
         return maxScaleLen < 3 ? 0 : maxScaleLen;
